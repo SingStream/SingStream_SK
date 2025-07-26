4ef5d640648fd31e647a755c2005723c093175a7
SingStream <sp212373@gmail.com>
2025-07-26 21:25:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -228,3 +228,19 @@ searchInput.addEventListener("keydown", e => {
 // -------------------- 8. INIT --------------------
 loadSong(index);highlightCurrent();
 
+let menu_list_active_button = document.getElementById('menu_list_active_button');
+let menu_side = document.getElementsByClassName('menu_side')[0];
+
+menu_list_active_button.addEventListener('click', () => {
+  menu_side.style.transform = "unset";
+  menu_list_active_button.style.opacity = 0;
+
+} )
+
+let song_side = document.getElementsByClassName('song_side')[0];
+  
+song_side.addEventListener('click', () => {
+  menu_side.style.transform = "translateX(-100%)";
+  menu_list_active_button.style.opacity = 1;
+
+} )
